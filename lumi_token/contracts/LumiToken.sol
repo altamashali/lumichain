bfaa70ff96d65d8b538cc75e4273ba937d4f8810
Altamash Ali <altamashali@live.com>
2020-06-15 20:35:24 -0500

    3. Delegated Transfer

--- lumi_token/contracts/LumiToken.sol
+++ lumi_token/contracts/LumiToken.sol
@@ -17,7 +17,14 @@ contract LumiToken{
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
     mapping(address => uint256) public balanceOf;
+    mapping(address => mapping(address => uint256)) public allowance; // allowance: A approves B to spend C
 
     constructor (uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
@@ -37,5 +44,38 @@ contract LumiToken{
         return true;
     }
 
+    // *** Delegated Transfer ***
+    // approve
+    function approve(address _spender, uint256 _value) public returns (bool success){
+        // allowance
+        allowance[msg.sender][_spender] = _value;
+
+        // Approve event
+        emit Approval(msg.sender, _spender, _value);
+
+        return true;
+    }
+
+    // transferFrom
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){
+        // Require _from has enough tokens
+        require(_value <= balanceOf[_from], 'has enough tokens');
+
+        // Require allowance is big enough
+        require(_value <= allowance[_from][msg.sender], 'allowance is big enough');
+
+        // Change the balance
+        balanceOf[_from] -= _value;
+        balanceOf[_to] += _value;
+
+        // Update the allowance
+        allowance[_from][msg.sender] -= _value;
+
+        // Transfer event
+        emit Transfer(_from, _to, _value);
+
+        // return a boolean
+        return true;
+    }
 
 }
